036de6dc281ad3b596dd2d985802bfbe1a9fd9ab
CarlosMarpaung <119844442+Miracle080604@users.noreply.github.com>
2023-12-10 18:47:24 +0700

    Push all source code
    Push all source code

---
+++ Proyek Library/src/librarymanagement/Database.java
@@ -0,0 +1,19 @@
+package librarymanagement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Database {
+    
+    public static Connection connectDB(){
+        try{
+        Class.forName("com.mysql.jdbc.Driver");
+//        MY DATABASE IS marcoData
+        Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/pbo_db", "root", "");
+            return connect;
+        }catch(Exception e){e.printStackTrace();}
+        
+        return null;
+    }
+    
+}
